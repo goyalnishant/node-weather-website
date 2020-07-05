b3b7149ba2d463e6dce49243d9126f01493727ad
Nishant Goyal <goyalnishant>
2020-07-05 21:47:05 +0530

    Remove unnecessary console log commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Welcome to Client side javascript')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const m1 = document.querySelector('#m1')
